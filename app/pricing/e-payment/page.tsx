fb73ff6905ec2d8edccfe3ac2cc474800f58c67c
BMBachir <boubaidjabachir3@gmail.com>
2025-05-03 17:18:20 +0100

    agaiiiiin

--- app/pricing/e-payment/page.tsx
+++ app/pricing/e-payment/page.tsx
@@ -1,9 +1,10 @@
+import { Suspense } from "react";
 import PaymentPage from "./components/PaymentPageClient";
 
 export default function page() {
   return (
-    <div>
+    <Suspense fallback={<div>Loading...</div>}>
       <PaymentPage />
-    </div>
+    </Suspense>
   );
 }
